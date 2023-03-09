096e8a71a5062d0546b94084878112929e7cfdcd
Poven45 <Povensangha@ymail.com>
2023-03-09 01:56:40 -0800

    Added button function and changed styling

--- my-app.js
+++ my-app.js
@@ -1,12 +1,29 @@
+/* 
 const hourEL = document.getElementById("hour");
 const minuteEl = document.getElementById ("minutes");
 const secondEl = document.getElementById ("seconds");
 const amEl = document.getElementById("am");
+*/
+
+//for making the physical clock
 const hourHand = document.querySelector('[data-hour-hand]');
 const minuteHand = document.querySelector('[data-minute-hand]');
 const secondHand = document.querySelector('[data-second-hand]');
 
+let boolean_Var = false;
+
 setInterval(setClock, 1000)
+
+function checkButton(){
+    boolean_Var = !boolean_Var;
+    if(boolean_Var == true) {
+        document.getElementById("military-time").value="12 Hr";
+        
+    } else if(boolean_Var == false){
+        document.getElementById("military-time").value="24 Hr";
+    }
+    
+}
 
 function setClock() {
     const currentDate = new Date();
@@ -22,32 +39,66 @@ function setRotation(element, rotationRatio) {
     element.style.setProperty("--rotation", rotationRatio * 360);
 }
 
-function updateClock(){
-    let h = new Date().getHours();
-    let m = new Date().getMinutes();
-    let s = new Date().getSeconds();
+function clock24() {
+    var today = new Date();
+    var hour = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
     let am = "AM";
 
-    if (h > 12){
-        h = h - 12;
-       am = "PM";
+    if(hour >= 12){
+        am = "PM";
+    }
+    
+    if(hour < 10){
+        hour = "0" + hour;
+    }
+    if(minutes < 10){
+        minutes = "0" + minutes;
+    }
+    if(seconds < 10){
+        seconds = "0" + seconds;
     }
 
-    h = h<10 ? "0" + h : h;
-    m = m<10 ? "0" + m : m;
-    s = s<10 ? "0" + s : s;
-
-
-    hourEL.innerText = h;
-    minuteEl.innerText = m;
-    secondEl.innerText = s
-    amEl.innerText = am;
-
-    setTimeout(()=>{
-updateClock()
-    },1000);
+    if(boolean_Var == false){
+        hour = hour > 12 ? /*if <--statement is true*/hour % 12 : /*if <--statement is false*/ hour;
+    }
+    document.querySelector("#hour").innerHTML = hour;
+    document.querySelector("#minutes").innerHTML = minutes;
+    document.querySelector("#seconds").innerHTML = seconds;
+    document.querySelector("#am").innerHTML = am;
 }
+/* 
+function clock12() {
+    var today = new Date();
+    var hour = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+    let am = "AM";
+
+    if(hour >= 12){
+        am = "PM";
+    }
+
+    if(hour < 10){
+        hour = "0" + hour;
+    }
+    
+    if(minutes < 10){
+        minutes = "0" + minutes;
+    }
+    if(seconds < 10){
+        seconds = "0" + seconds;
+    }
+    */
+
+    
+
+    document.querySelector("#hour").innerHTML = hour;
+    document.querySelector("#minutes").innerHTML = minutes;
+    document.querySelector("#seconds").innerHTML = seconds;
+    document.querySelector("#am").innerHTML = am;
 
 
-updateClock();
 setClock();
+let updateClock = setInterval(clock24, 1000);
